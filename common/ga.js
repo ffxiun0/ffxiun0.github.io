36fb90287dc58997eab88450eb72c3aa950ec5d9
ffxiun0 <ffxiun0@gmail.com>
2022-04-11 18:43:11 +0900

    GA

--- common/ga.js
+++ common/ga.js
@@ -1,19 +1,3 @@
-function gaInjectScript(src) {
-    var is = document.createElement('script');
-    is.async = true;
-    is.src = src;
-
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(is, s);
-};
-
-// Global site tag(gtag.js) - Google Analytics
-gaInjectScript('https://www.googletagmanager.com/gtag/js?id=UA-63365158-2');
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-63365158-2');
-
 // Google Tag Manager
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
